deb09bcd5605fa4b80afa55ed368e00f1eb2546c
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-09 19:14:50 +0700

    feat: add TipeDataInteger.php

---
+++ TipeDataInteger.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Deciumal: ";
+var_dump(1234); // int(1234)
+
+echo "Octal: ";
+var_dump(0123); // int(83)
+
+echo "Hexadecimal: ";
+var_dump(0x1A); // int(26)
+
+echo "Binary: ";
+var_dump(0b11111111); // int(255)
+
+echo "Decimal: ";
+var_dump(1234); // int(1234)
+
+// Agar mudah dibaca dapat menggunakan underscore
+echo "Underscore di Number: ";
+var_dump(1_234_567); // int(1234567)
