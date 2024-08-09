672f8f83aa2f971f29fa93abc5faf9b5118b5eed
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-09 20:09:31 +0700

    feat: add VariableVariables.php

---
+++ VariableVariables.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "naufal";
+$$name = "fadhil";
+$$$name = "athallah";
+
+echo "\$name: ";
+echo $name; // naufal
+
+echo "\n";
+
+echo "\$naufal: ";
+echo $naufal; // fadhil
+
+echo "\n";
+
+echo "\$fadhil: ";
+echo $fadhil; // athallah
