79cd62aee23525bc4d5bb3281ffc92ed08d21aef
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-09 19:15:32 +0700

    feat: add TipeDataFloatingPoint.php

---
+++ TipeDataFloatingPoint.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo "Floating Point: ";
+var_dump(1.234); // float(1.234)
+
+echo "Floating Point dengan E natation Plus (1.2 x 1000): ";
+var_dump(1.2e3); // float(1200)
+
+echo "Floating Point dengan E notation Minus (7 x 0.001): ";
+var_dump(7e-3); // float(0.007)
+
+echo "Underscore di Floating Point: ";
+var_dump(1_234.267); // float(1234.267)
