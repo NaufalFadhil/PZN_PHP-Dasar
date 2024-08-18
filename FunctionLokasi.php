6b943c69d036a68c771a9a6e4b34e805bd2607cb
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-18 12:28:16 +0700

    feat: add Function and FunctionLokasi

---
+++ FunctionLokasi.php
@@ -0,0 +1,11 @@
+<?php
+
+$buatFunction = true; // Jika false maka function tidak akan dibuat dan error
+
+if ($buatFunction) {
+  function hi() {
+    echo "Hi Function" . PHP_EOL;
+  }
+}
+
+hi(); // Hi Function
