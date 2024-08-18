0307fdeb213e28154b57b4158f2c92ca101b19df
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-08-18 17:23:29 +0700

    feat: add Komentar

---
+++ Komentar.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ini adalah komentar multi baris 
+yang bisa digunakan untuk memberikan penjelasan
+panjang pada kode
+*/
+function sayHello(){
+  // ini adalah komentar satu baris
+  echo "Hello" . PHP_EOL;
+}
+
+sayHello(); // Hello
